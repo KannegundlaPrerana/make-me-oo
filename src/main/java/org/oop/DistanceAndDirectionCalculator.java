2708d449bec89ae3a6926925adfe536afae9b700
KannegundlaPrerana <preranakannegundla4@gmail.com>
2020-08-16 14:47:13 +0530

    Use getXDistance, getYDistance methods

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -2,14 +2,14 @@ package org.oop;
 
 public class DistanceAndDirectionCalculator {
     public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
+        double xDistance = to.getXDistance(from);
+        double yDistance = to.getYDistance(from);
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
+        double xDistance = to.getXDistance(from);
+        double yDistance = to.getYDistance(from);
         return Math.atan2(yDistance, xDistance);
     }
 }
